6796d9c002dfb5f546731fb70e7a1500f42c8d52
Simon <71475325+dnwa-simon@users.noreply.github.com>
2024-07-10 15:31:01 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 # st.stop()
 
 pd_df = my_dataframe.to_pandas()
+pd_df.print()
 # st.dataframe(pd_df)
 # st.stop()
 
