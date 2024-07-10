82e15b097de22517942bc89866b2fc7b66f21672
Simon <71475325+dnwa-simon@users.noreply.github.com>
2024-07-10 15:34:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ session = cnx.session()
 name_on_order = st.text_input("Name on your smoothie")
 st.write("The name on your smoothie will be :", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
 
 pd_df = my_dataframe.to_pandas()
 pd_df.print()
@@ -28,7 +28,7 @@ pd_df.print()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
-    , max_selections=6
+    , max_selections=5
 )
 
 if ingredients_list:
